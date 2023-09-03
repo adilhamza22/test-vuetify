9752fe33b990ea67b35df03a0455d8319150793f
adilhamza22 <l191121@lhr.nu.edu.pk>
2023-09-03 12:47:58 +0500

    Vuex + Vuetify

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="signup-main">
+    <SignUpForm />
+  </div>
+</template>
+
+<script>  
+import SignUpForm from "@/components/SignUpForm.vue";
+  export default {
+    components: {
+      SignUpForm,
+    },
+    
+  }
+</script>
+
+<style scoped>
+
+.signup-main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: linear-gradient(to right, #fbc2eb 0%, #a6c1ee 100%);
+}
+</style>
